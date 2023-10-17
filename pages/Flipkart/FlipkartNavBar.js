2d0c691070a5b589ab87cf555ab8c0878fb5f8c1
Abhijeet kumar <abhijeet122kumar@gmail.com>
2023-10-17 12:58:19 +0530

    Working on seachAlgo API

--- pages/Flipkart/FlipkartNavBar.js
+++ pages/Flipkart/FlipkartNavBar.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Style from "/styles/Flipkart.module.css";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
@@ -6,6 +6,18 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import Link from "next/link";
 import Head from 'next/head';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function FlipkartNavBar() {
     const handleDownload = useCallback(() => {
@@ -14,6 +26,52 @@ function FlipkartNavBar() {
         link.download = "/Image/AbhijeetKumar_SE.pdf";
         link.click();
     });
+    const [open, setOpen] = React.useState(false);
+    const [getQuestion, setQuestion] = useState('')
+    const [getAnswerfromPDF, setGetAnswerfromPDF] = useState('')
+    const handleClickOpen = () => {
+
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const onQuestionInput = (e) => {
+        setQuestion(e.target.value)
+    }
+   
+    const findAnswerFromPDF = () => {
+        setOpen(true);
+        console.log(true);
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "user_input": "and what programming languages you used"
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            mode: 'no-cors',
+            
+        };
+
+        fetch("http://65.0.45.74:5000/get_answer", requestOptions)
+        .then(response => {
+            console.log('Response:', response);
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+        })
+        .then(result => setGetAnswerfromPDF(result.answer))
+        .catch(error => console.log('error', error));
+
+    }
 
     return (
         <>
@@ -32,7 +90,7 @@ function FlipkartNavBar() {
                     >
                         Abhijeet Kumar
                     </Link>
-                    <button
+                    <Button
                         className="navbar-toggler"
                         type="button"
                         data-bs-toggle="collapse"
@@ -42,7 +100,7 @@ function FlipkartNavBar() {
                         aria-label="Toggle navigation"
                     >
                         <span className="navbar-toggler-icon"></span>
-                    </button>
+                    </Button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
                             <li className="nav-item">
@@ -58,6 +116,7 @@ function FlipkartNavBar() {
                                     >
                                         <InputBase
                                             sx={{ ml: 1, flex: 1 }}
+                                            onChange={(e) => onQuestionInput(e)}
                                             placeholder="Soon will add search Algorithum"
                                             inputProps={{ "aria-label": "search google maps" }}
                                         />
@@ -65,7 +124,9 @@ function FlipkartNavBar() {
                                             type="button"
                                             sx={{ p: "10px" }}
                                             aria-label="search"
+                                            onClick={findAnswerFromPDF}
                                         >
+
                                             <SearchIcon />
                                         </IconButton>
                                     </Paper>
@@ -84,7 +145,27 @@ function FlipkartNavBar() {
                         </span>
                     </button>
                 </div>
+
             </nav>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        {getAnswerfromPDF}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose}>Agree</Button>
+                </DialogActions>
+            </Dialog>
+
         </>
     );
 }
